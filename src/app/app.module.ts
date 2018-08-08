7a5a3b1612e14380e34f2e1e3522bacbc8e0380a
vorajeem <vorajeem@gmail.com>
2018-08-08 15:34:14 +0200

    Added home,footer,navbar,product-item,product-detail,search components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductItemComponent,
+    ProductDetailComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule

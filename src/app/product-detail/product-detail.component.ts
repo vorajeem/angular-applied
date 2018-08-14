f8ed70bb938f72fad0200eece618185e07ce3966
vorajeem <vorajeem@gmail.com>
2018-08-14 11:44:30 +0200

    Added Product Detail component functionality

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../shared/product.service';
+import {Product} from '../shared/product';
+import {ActivatedRoute} from '@angular/router';
+
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  product: Product;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit() {
+    let prodId: number = parseInt( this.route.snapshot.params['productId']);
+    this.product = this.productService.getProductById(prodId);
   }
 
 }
